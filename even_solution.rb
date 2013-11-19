b2e59426c813df8b63fd7c2c1f98de48797f2134
Amine TheDream <aminethedream@new-host-19.home>
2013-11-19 09:20:15 -0500

    add files

---
+++ even_solution.rb
@@ -0,0 +1,9 @@
+def first_even(items)
+  print_first_even = []
+  first_even[].each do |element|
+  	if element % 2 == 0 
+  		print_first_even <<  element
+  	end
+  	return print_first_even[]
+
+end
